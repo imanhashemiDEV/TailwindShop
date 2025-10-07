3ccbc0c354153e86c42eaf78d8bf4e493bfcbe9f
iman hashemi <hashemi.iman@gmail.com>
2025-10-07 18:54:07 +0330

    header

--- src/pages/scripts/scripts.js
+++ src/pages/scripts/scripts.js
@@ -1,17 +1,21 @@
 
-document.getElementById('main').addEventListener('mouseover',function(){  
-document.getElementById("menu").firstElementChild.lastElementChild.classList.remove('opacity-0')
-document.getElementById("menu").firstElementChild.lastElementChild.classList.remove('invisible')
-document.getElementById("menu").firstElementChild.lastElementChild.classList.add('visible')
-document.getElementById("menu").firstElementChild.lastElementChild.classList.add('opacity-100')
-})
+let categories = document.getElementById('categories')
+let categories_list = document.getElementById('categories_list')
+let dark_menu_background = document.getElementById('dark_menu_background')
 
-document.getElementById('main').addEventListener('mouseleave',function(){  
-document.getElementById("menu").firstElementChild.lastElementChild.classList.add('opacity-0')
-document.getElementById("menu").firstElementChild.lastElementChild.classList.add('invisible')
-document.getElementById("menu").firstElementChild.lastElementChild.classList.remove('visible')
-document.getElementById("menu").firstElementChild.lastElementChild.classList.remove('opacity-100')
-})
+categories.addEventListener('mouseover',openMenu)
+categories.addEventListener('mouseleave',closeMenu)
+dark_menu_background.addEventListener('mousemove',closeMenu)
+ 
 
+function openMenu(){
+    categories_list.classList.remove('hidden')
+    categories_list.classList.add('flex')
+    document.body.style.overflow='hidden'
+}
 
-
+function closeMenu(){
+    categories_list.classList.remove('flex')
+    categories_list.classList.add('hidden')
+    document.body.style.overflow='auto'
+}
